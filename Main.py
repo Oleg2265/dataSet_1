6a2e30ef91330d2d819cf6a4f2e101c75906c44d
meimeuiii <makariakmaksum@gmail.com>
2024-11-05 17:25:00 +0200

    last

--- Main.py
+++ Main.py
@@ -1,11 +1,28 @@
 import pandas as pd
-df = pd.read_csv('countries of the world.csv')
-#print(df.info())
-print(pd.isnull(df["Net migration"]))
-df["Net migration"].fillna(0, inplace = True)
-print(pd.isnull(df["Net migration"]))
 
-def make_migration(migration):
-    if migration[-1] == "-":
-        return float(migration[1:])
-    return -1
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('countries of the world.csv')
+
+#df["Population"].mean()
+#print(df["Population"].mean())
+#28,740,284.7
+
+
+
+
+df["Area (sq. mi.)"].mean()
+print(df["Area (sq. mi.)"].mean())
+#598227
+
+df["Area (sq. mi.)"].value_counts().plot(kind = 'pie')
+
+
+
+
+
+#def make_pop(Population):
+    #if Population[-1] == "-":
+        #return float(Population[1:])
+    #return -1
+
